d5431672a03cbeec5657b2ecda55b1f39f1376ff
TEKami13 <tobias.krogh@hotmail.com>
2024-09-27 09:53:35 +0200

    first commit

---
+++ src/main/java/org/example/devdemo/DevdemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.devdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/devdemo/DevdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
